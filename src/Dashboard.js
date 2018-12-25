d3a54e3c3a0ace95f4180570c84eec23ce7a5607
mahdi <mahdi.ebrahimi@codecentric.de>
2018-12-25 16:23:35 +0100

    added standard JavaScript Style, fixed violations

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,28 +1,23 @@
-'use strict';
+'use strict'
 
 module.exports = class Dashboard {
+  create (region, metric, stats, functions) {
+    const Widget = require('./Widget')
+    const widgetFactory = new Widget()
 
-    create(region, metric, stats, functions) {
-        const dashboardName = `${metric}Dashboard`;
+    const dashboardName = `${metric}Dashboard`
+    const widgets = stats.map(stat => widgetFactory.perFunction(region, metric, stat, functions))
+    widgets.unshift(widgetFactory.acrossAll(region, metric, stats))
+    return Dashboard.dashboard(dashboardName, widgets)
+  }
 
-        const Widget = require('./Widget');
-        const widgetFactory = new Widget();
-
-        if (stats.length > 0) {
-            const widgets = stats.map(stat => widgetFactory.perFunction(region, metric, stat, functions));
-            widgets.unshift(widgetFactory.acrossAll(region, metric, stats));
-
-            return Dashboard.dashboard(dashboardName, widgets);
-        }
+  static dashboard (name, widgets) {
+    return {
+      Type: 'AWS::CloudWatch::Dashboard',
+      Properties: {
+        DashboardName: name,
+        DashboardBody: JSON.stringify({ widgets: widgets })
+      }
     }
-
-    static dashboard(name, widgets) {
-        return {
-            Type : "AWS::CloudWatch::Dashboard",
-            Properties : {
-                DashboardName : name,
-                DashboardBody : JSON.stringify({ widgets: widgets}),
-            }
-        };
-    }
-};
+  }
+}
--- src/DashboardPlugin.js
+++ src/DashboardPlugin.js
@@ -1,56 +1,79 @@
-'use strict';
+'use strict'
 
-module.exports = class DashboardPlugin {
-
-  constructor(serverless, options) {
-    const service = serverless.service;
-
-    const Dashboard = require('./Dashboard');
-    const dashboardFactory = new Dashboard();
-
+/**
+ * @module serverless-plugin-cloudwatch-dashboard
+ *
+ * @see {@link https://serverless.com/framework/docs/providers/aws/guide/plugins/}
+ * */
+class DashboardPlugin {
+  /**
+   * @description Serverless CloudWatch Dashboards for Lambdas
+   * @constructor
+   *
+   * @param {!Object} serverless - Serverless object
+   * @param {!Object} options - Serverless options
+   * */
+  constructor (serverless, options) {
+    const logger = msg => serverless.cli.log('[serverless-plugin-cloudwatch-dashboard]: ' + msg)
     this.hooks = {
-      'before:package:finalize': () => this.addDashboards(service, dashboardFactory),
-    };
+      'before:package:finalize': () => this.addDashboards(serverless.service, logger)
+    }
   }
 
-  addDashboards(service, dashboardFactory) {
-    const functions = DashboardPlugin.getFunctionNames(service);
-    const config = DashboardPlugin.getConfig(service);
+  addDashboards (service, logger) {
+    const config = DashboardPlugin.getConfig(service)
+    const functions = DashboardPlugin.getFunctionNames(service)
+    const stats = config.stats
+    const metrics = config.metrics
 
-    const dashboards = config.metrics.map(metric => dashboardFactory.create(
-        service.provider.region,
-        metric,
-        config.stats,
-        functions
-    ));
+    if (DashboardPlugin.notEmpty(functions, stats, metrics)) {
+      logger('Adding dashboards:')
+      const dashboards = this.createDashboards(service.provider.region, functions, metrics, stats)
+      const dashboardResources = dashboards.reduce(function (acc, next) {
+        logger('- ' + next.Properties.DashboardName)
+        acc[next.Properties.DashboardName] = next
+        return acc
+      }, {})
 
-    const template = service.provider.compiledCloudFormationTemplate;
-    template.Resources = template.Resources || {};
-
-    dashboards
-        .filter(v => typeof v !== 'undefined')
-        .forEach(dashboard => template.Resources[dashboard.Properties.DashboardName] = dashboard);
+      const template = service.provider.compiledCloudFormationTemplate
+      template.Resources = Object.assign(dashboardResources, template.Resources)
+    }
   }
 
-  static getConfig(service) {
-    const custom = service.custom || {};
-    const config = custom.dashboard || {};
+  createDashboards (region, functions, metrics, stats) {
+    const Dashboard = require('./Dashboard')
+    const dashboardFactory = new Dashboard()
+    return metrics.map(metric => dashboardFactory.create(region, metric, stats, functions))
+  }
+
+  static getConfig (service) {
+    const custom = service.custom || {}
+    const config = custom.dashboard || {}
 
     const defaultConfig = {
       metrics: ['Duration', 'Errors', 'Invocations', 'Throttles'],
       stats: ['p99', 'p95', 'p90', 'p50']
-    };
+    }
 
-    return Object.assign(defaultConfig, config);
+    return Object.assign(defaultConfig, config)
   }
 
-  static getFunctionNames(service) {
-    const functions = service.functions;
+  static getFunctionNames (service) {
+    const functions = service.functions
 
     return Object.keys(functions).map(name => {
-      const config = functions[name].dashboard;
-      if (config === false || config === 'false') return undefined;
-      else return name;
-    }).filter(v => typeof v !== 'undefined');
+      const config = functions[name].dashboard
+      if (config === false || config === 'false') return undefined
+      else return name
+    }).filter(v => typeof v !== 'undefined')
   }
-};
+
+  static notEmpty (...arrays) {
+    return arrays.map(array => typeof array !== 'undefined' && array.length > 0)
+      .reduce(function (acc, next) {
+        return acc && next
+      }, true)
+  }
+}
+
+module.exports = DashboardPlugin
