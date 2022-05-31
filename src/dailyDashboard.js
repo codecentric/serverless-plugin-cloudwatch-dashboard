992a635da68972943b9291757c17d81978373865
J Fernandes <JoaoRXFernandes@users.noreply.github.com>
2022-05-31 16:37:23 +0100

    Daily dashboard minor updates. (#2)

--- src/dailyDashboard.js
+++ src/dailyDashboard.js
@@ -1,20 +1,61 @@
 'use strict'
 
-module.exports = function createDailyDashboard(service) {
+module.exports = function createDailyDashboard(service, stage) {
   const region = service.provider.region
   const restApiName = service.provider.compiledCloudFormationTemplate.Resources["ApiGatewayRestApi"].Properties.Name
+  const functionsApiGatewayRoutes = Object.keys(service.functions).map(key => service.functions[key].events?.find(x => x.http).http).filter(x => x !== undefined)
 
-  const logGroupNames = Object.keys(service.provider.compiledCloudFormationTemplate.Resources)
+  const requestsAndErrorsMetrics = getRequestAndErrorsWidgetMetrics(stage, restApiName, functionsApiGatewayRoutes)
+  const responseTimesMetrics = getResponseTimesWidgetMetrics(stage, restApiName, functionsApiGatewayRoutes)
+
+  const logGroupSources = Object.keys(service.provider.compiledCloudFormationTemplate.Resources)
     .filter(x => x.endsWith("LogGroup"))
-    .map(key => service.provider.compiledCloudFormationTemplate.Resources[key].Properties.LogGroupName);
+    .map(key => `SOURCE '${service.provider.compiledCloudFormationTemplate.Resources[key].Properties.LogGroupName}'`)
+    .join(" | ");
 
   return {
     LambdaDailyDashboard: {
       Type: 'AWS::CloudWatch::Dashboard',
       Properties: {
-        DashboardName: "Daily",
-        DashboardBody: `{\"start\":\"-PT24H\",\"periodOverride\":\"inherit\",\"widgets\":[{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":0,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Requests\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"${restApiName}\",{\"label\":\" \",\"period\":86400,\"stat\":\"Sum\"}]]}},{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":6,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Errors\",\"region\":\"${region}\",\"metrics\":[[{\"label\":\" \",\"color\":\"#E42A2A\",\"expression\":\"SUM([serverErrors, clientErrors])\",\"period\":86400}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"${restApiName}\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"serverErrors\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"${restApiName}\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"clientErrors\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":3,\"x\":12,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Response Time (p95)\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",{\"label\":\" \",\"period\":86400,\"stat\":\"p95\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":0,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Requests / Errors\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"${restApiName}\",{\"color\":\"#1f77b4\",\"label\":\"Requests\",\"period\":3600,\"stat\":\"Sum\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"${restApiName}\",{\"color\":\"#ff7f0e\",\"label\":\"4XXs\",\"period\":3600,\"stat\":\"Sum\",\"yAxis\":\"right\"}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"${restApiName}\",{\"color\":\"#d13212\",\"label\":\"5XXs\",\"period\":3600,\"stat\":\"Sum\",\"yAxis\":\"right\"}]],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":12,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Response Time\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",{\"label\":\"Minimum\",\"period\":3600,\"stat\":\"Minimum\"}],[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",{\"label\":\"p95\",\"period\":3600,\"stat\":\"p95\"}],[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",{\"label\":\"Maximum\",\"period\":3600,\"stat\":\"Maximum\"}]],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"log\",\"width\":24,\"height\":6,\"x\":0,\"y\":9,\"properties\":{\"view\":\"table\",\"title\":\"Errors\",\"region\":\"${region}\",\"query\":\"SOURCE [${logGroupNames}] | fields fromMillis(@timestamp) as Date, statusCode as Status, concat(subDomain, \\\".\\\", boundedContext, \\\".\\\", functionName) as Function, coalesce(message, @message) as Message, error.message as Detail, httpRequestId as RequestId\\n| filter statusCode >= 500 or @message like \\\"WafFiltered\\\"\"}}]}`
+        DashboardName: `${service.service}-daily`,
+        DashboardBody: `{\"start\":\"-PT24H\",\"periodOverride\":\"inherit\",\"widgets\":[{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":0,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Requests\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"${restApiName}\",{\"label\":\" \",\"period\":86400,\"stat\":\"Sum\"}]]}},{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":6,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Errors\",\"region\":\"${region}\",\"metrics\":[[{\"label\":\" \",\"color\":\"#E42A2A\",\"expression\":\"SUM([serverErrors, clientErrors])\",\"period\":86400}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"${restApiName}\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"serverErrors\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"${restApiName}\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"clientErrors\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":3,\"x\":12,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Response Time (p95)\",\"region\":\"${region}\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",{\"label\":\" \",\"period\":86400,\"stat\":\"p95\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":0,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Requests / Errors\",\"region\":\"${region}\",\"period\":3600,\"stat\":\"Sum\",\"metrics\":[${requestsAndErrorsMetrics.join(",")}],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":12,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Response Times\",\"region\":\"${region}\",\"period\":3600,\"stat\":\"p95\",\"metrics\":[${responseTimesMetrics.join(",")}],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"log\",\"width\":24,\"height\":6,\"x\":0,\"y\":9,\"properties\":{\"view\":\"table\",\"title\":\"Errors\",\"region\":\"${region}\",\"query\":\"${logGroupSources} | fields fromMillis(@timestamp) as Date, statusCode as Status, concat(subDomain, \\\".\\\", boundedContext, \\\".\\\", functionName) as Function, coalesce(message, @message) as Message, error.message as Detail, httpRequestId as RequestId\\n| filter statusCode >= 500 or @message like \\\"WafFiltered\\\"\"}}]}`
       }
     }
   }
+}
+
+function getRequestAndErrorsWidgetMetrics(stage, restApiName, functionsApiGatewayRoutes) {
+  const requestsAndErrorsMetrics = [];
+
+  for (var i = 0; i < functionsApiGatewayRoutes.length; i++) {
+    if (i === 0) {
+      requestsAndErrorsMetrics.push(`[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"${restApiName}\",\"Resource\",\"/${functionsApiGatewayRoutes[i].path}\",\"Stage\",\"${stage}\",\"Method\",\"${functionsApiGatewayRoutes[i].method}\"]`);
+    } else {
+      requestsAndErrorsMetrics.push(`[\"...\",\"/${functionsApiGatewayRoutes[i].path}\", \".\", \".\", \".\", \"${functionsApiGatewayRoutes[i].method}\"]`);
+    }
+  }
+
+  for (var i = 0; i < functionsApiGatewayRoutes.length; i++) {
+    if (i === 0) {
+      requestsAndErrorsMetrics.push(`[\".\",\"5XXError\",\".\",\".\",\".\",\"/${functionsApiGatewayRoutes[i].path}\",\".\",\".\",\".\",\"${functionsApiGatewayRoutes[i].method}\",{ "yAxis": "right" }]`);
+    } else {
+      requestsAndErrorsMetrics.push(`[\"...\",\"/${functionsApiGatewayRoutes[i].path}\", \".\", \".\", \".\", \"${functionsApiGatewayRoutes[i].method}\"],{ "yAxis": "right" }`);
+    }
+  }
+
+  return requestsAndErrorsMetrics;
+}
+
+function getResponseTimesWidgetMetrics(stage, restApiName, functionsApiGatewayRoutes) {
+  const responseTimesMetrics = [];
+
+  for (var i = 0; i < functionsApiGatewayRoutes.length; i++) {
+    if (i === 0) {
+      responseTimesMetrics.push(`[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"${restApiName}\",\"Resource\",\"/${functionsApiGatewayRoutes[i].path}\",\"Stage\",\"${stage}\",\"Method\",\"${functionsApiGatewayRoutes[i].method}\"]`);
+    } else {
+      responseTimesMetrics.push(`[\"...\",\"/${functionsApiGatewayRoutes[i].path}\", \".\", \".\", \".\", \"${functionsApiGatewayRoutes[i].method}\"]`);
+    }
+  }
+
+  return responseTimesMetrics;
 }
--- tests/dailyDashboard.spec.js
+++ tests/dailyDashboard.spec.js
@@ -21,21 +21,44 @@ describe("given a lambda", () => {
                             Type: "AWS::Logs::LogGroup",
                             Properties: {
                                 LogGroupName: "/aws/lambda/service-dev-foo",
-                                RetentionInDays: 400
+                            }
+                        },
+                        barLogGroup: {
+                            Type: "AWS::Logs::LogGroup",
+                            Properties: {
+                                LogGroupName: "/aws/lambda/service-dev-bar",
                             }
                         },
                         ApiGatewayLogGroup: {
                             Type: "AWS::Logs::LogGroup",
                             Properties: {
                                 LogGroupName: "/aws/api-gateway/service-dev",
-                                RetentionInDays: 400
                             }
                         }
                     }
                 }
             },
             functions: {
-                foo: {}
+                foo: {
+                    events: [
+                        {
+                            http: {
+                                method: "get",
+                                path: "foo"
+                            }
+                        }
+                    ]
+                },
+                bar: {
+                    events: [
+                        {
+                            http: {
+                                method: "post",
+                                path: "bar"
+                            }
+                        }
+                    ]
+                }
             }
         }
     }
@@ -54,33 +77,37 @@ describe("given a lambda", () => {
         plugin.addDashboards()
         expect(serverless.service.provider.compiledCloudFormationTemplate).toEqual({
             "Resources": {
-                "ApiGatewayLogGroup": {
-                    "Type": "AWS::Logs::LogGroup",
+                "LambdaDailyDashboard": {
+                    "Type": "AWS::CloudWatch::Dashboard",
                     "Properties": {
-                        "LogGroupName": "/aws/api-gateway/service-dev",
-                        "RetentionInDays": 400,
+                        "DashboardName": "service-daily",
+                        "DashboardBody": "{\"start\":\"-PT24H\",\"periodOverride\":\"inherit\",\"widgets\":[{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":0,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Requests\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"rest-api-name\",{\"label\":\" \",\"period\":86400,\"stat\":\"Sum\"}]]}},{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":6,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Errors\",\"region\":\"us-east-1\",\"metrics\":[[{\"label\":\" \",\"color\":\"#E42A2A\",\"expression\":\"SUM([serverErrors, clientErrors])\",\"period\":86400}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"rest-api-name\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"serverErrors\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"rest-api-name\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"clientErrors\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":3,\"x\":12,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Response Time (p95)\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",{\"label\":\" \",\"period\":86400,\"stat\":\"p95\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":0,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Requests / Errors\",\"region\":\"us-east-1\",\"period\":3600,\"stat\":\"Sum\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"rest-api-name\",\"Resource\",\"/foo\",\"Stage\",\"dev\",\"Method\",\"get\"],[\"...\",\"/bar\", \".\", \".\", \".\", \"post\"],[\".\",\"5XXError\",\".\",\".\",\".\",\"/foo\",\".\",\".\",\".\",\"get\",{ \"yAxis\": \"right\" }],[\"...\",\"/bar\", \".\", \".\", \".\", \"post\"],{ \"yAxis\": \"right\" }],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":12,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Response Times\",\"region\":\"us-east-1\",\"period\":3600,\"stat\":\"p95\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",\"Resource\",\"/foo\",\"Stage\",\"dev\",\"Method\",\"get\"],[\"...\",\"/bar\", \".\", \".\", \".\", \"post\"]],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"log\",\"width\":24,\"height\":6,\"x\":0,\"y\":9,\"properties\":{\"view\":\"table\",\"title\":\"Errors\",\"region\":\"us-east-1\",\"query\":\"SOURCE '/aws/lambda/service-dev-foo' | SOURCE '/aws/lambda/service-dev-bar' | SOURCE '/aws/api-gateway/service-dev' | fields fromMillis(@timestamp) as Date, statusCode as Status, concat(subDomain, \\\".\\\", boundedContext, \\\".\\\", functionName) as Function, coalesce(message, @message) as Message, error.message as Detail, httpRequestId as RequestId\\n| filter statusCode >= 500 or @message like \\\"WafFiltered\\\"\"}}]}"
                     }
                 },
                 "ApiGatewayRestApi": {
                     "Type": "AWS::ApiGateway::RestApi",
                     "Properties": {
-                        "Name": "rest-api-name",
+                        "Name": "rest-api-name"
                     }
                 },
                 "fooLogGroup": {
                     "Type": "AWS::Logs::LogGroup",
                     "Properties": {
-                        "LogGroupName": "/aws/lambda/service-dev-foo",
-                        "RetentionInDays": 400,
+                        "LogGroupName": "/aws/lambda/service-dev-foo"
                     }
                 },
-                "LambdaDailyDashboard": {
-                    "Type": "AWS::CloudWatch::Dashboard",
+                "barLogGroup": {
+                    "Type": "AWS::Logs::LogGroup",
                     "Properties": {
-                        "DashboardName": "Daily",
-                        "DashboardBody": "{\"start\":\"-PT24H\",\"periodOverride\":\"inherit\",\"widgets\":[{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":0,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Requests\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"rest-api-name\",{\"label\":\" \",\"period\":86400,\"stat\":\"Sum\"}]]}},{\"type\":\"metric\",\"width\":6,\"height\":3,\"x\":6,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Errors\",\"region\":\"us-east-1\",\"metrics\":[[{\"label\":\" \",\"color\":\"#E42A2A\",\"expression\":\"SUM([serverErrors, clientErrors])\",\"period\":86400}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"rest-api-name\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"serverErrors\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"rest-api-name\",{\"period\":86400,\"stat\":\"Sum\",\"visible\":false,\"id\":\"clientErrors\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":3,\"x\":12,\"y\":0,\"properties\":{\"view\":\"singleValue\",\"title\":\"Response Time (p95)\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",{\"label\":\" \",\"period\":86400,\"stat\":\"p95\"}]]}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":0,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Requests / Errors\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Count\",\"ApiName\",\"rest-api-name\",{\"color\":\"#1f77b4\",\"label\":\"Requests\",\"period\":3600,\"stat\":\"Sum\"}],[\"AWS/ApiGateway\",\"4XXError\",\"ApiName\",\"rest-api-name\",{\"color\":\"#ff7f0e\",\"label\":\"4XXs\",\"period\":3600,\"stat\":\"Sum\",\"yAxis\":\"right\"}],[\"AWS/ApiGateway\",\"5XXError\",\"ApiName\",\"rest-api-name\",{\"color\":\"#d13212\",\"label\":\"5XXs\",\"period\":3600,\"stat\":\"Sum\",\"yAxis\":\"right\"}]],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"metric\",\"width\":12,\"height\":6,\"x\":12,\"y\":3,\"properties\":{\"view\":\"timeSeries\",\"title\":\"Response Time\",\"region\":\"us-east-1\",\"metrics\":[[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",{\"label\":\"Minimum\",\"period\":3600,\"stat\":\"Minimum\"}],[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",{\"label\":\"p95\",\"period\":3600,\"stat\":\"p95\"}],[\"AWS/ApiGateway\",\"Latency\",\"ApiName\",\"rest-api-name\",{\"label\":\"Maximum\",\"period\":3600,\"stat\":\"Maximum\"}]],\"yAxis\":{\"left\":{\"min\":0},\"right\":{\"min\":0}}}},{\"type\":\"log\",\"width\":24,\"height\":6,\"x\":0,\"y\":9,\"properties\":{\"view\":\"table\",\"title\":\"Errors\",\"region\":\"us-east-1\",\"query\":\"SOURCE [/aws/lambda/service-dev-foo,/aws/api-gateway/service-dev] | fields fromMillis(@timestamp) as Date, statusCode as Status, concat(subDomain, \\\".\\\", boundedContext, \\\".\\\", functionName) as Function, coalesce(message, @message) as Message, error.message as Detail, httpRequestId as RequestId\\n| filter statusCode >= 500 or @message like \\\"WafFiltered\\\"\"}}]}"
+                        "LogGroupName": "/aws/lambda/service-dev-bar"
                     }
                 },
+                "ApiGatewayLogGroup": {
+                    "Type": "AWS::Logs::LogGroup",
+                    "Properties": {
+                        "LogGroupName": "/aws/api-gateway/service-dev"
+                    }
+                }
             }
         })
     })
